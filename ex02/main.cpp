2e32be386acca864789f6c40c826b4c3c35b63d7
Giulia Galizoni Caversan <ggalizon@c3a8c5.42berlin.de>
2025-11-18 12:17:42 +0100

    ex02 formated

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,7 +1,7 @@
 #include <string>
 #include <iostream>
 
-int	main()
+int main()
 {
 	std::string str = "HI THIS IS BRAIN";
 	std::string *stringPTR = &str;
@@ -9,7 +9,8 @@ int	main()
 
 	std::cout << "this is the memory address of the string variable: " << &str << std::endl;
 	std::cout << "this is the memory address held by stringPTR: " << stringPTR << std::endl;
-	std::cout << "this is the memory address held by stringREF: " << &stringREF << std::endl << std::endl;
+	std::cout << "this is the memory address held by stringREF: " << &stringREF << std::endl
+			  << std::endl;
 
 	std::cout << "this the value of the string variable: " << str << std::endl;
 	std::cout << "this is the value pointed to by stringPTR: " << *stringPTR << std::endl;
